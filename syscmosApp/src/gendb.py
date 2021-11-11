fdbb90be122b501c47f2bbf48f04986af2f929b2
Iain Marcuson <iain.marcuson@sydortechnologies.com>
2021-11-11 16:15:30 -0500

    Preliminary add of param database generation.

--- syscmosApp/src/gendb.py
+++ syscmosApp/src/gendb.py
@@ -7,11 +7,13 @@ if len(sys.argv) != 2:
     
 template_filename = sys.argv[1]
 query_db_filename = "gc_query.db"; # GenCam query database
+param_db_filename = "gc_params.db"; # GenCam parameter database
 
 query_suffix = "_Q"
 
 template_file = open(template_filename, "r");
 query_db_file = open(query_db_filename, "w");
+param_db_file = open(param_db_filename, "w");
 
 for curr_line in template_file:
     if ('#' in curr_line) and not curr_line.startswith('#'):
@@ -52,7 +54,51 @@ for curr_line in template_file:
     field_line = '}\n\n';
     query_db_file.write(field_line);
 
-    continue;
+    if pv_inhibit == 0:         # Generate new parameter PVs
+        record_type = ["", ""];
+        record_dtyp = "";
+        record_suffix = ["", "_RBV"]; # Suffix for setters and getters
+        if pv_type == 'd':
+            record_type = ["ao", "ai"];
+            record_dtyp = "asynFloat64";
+        elif pv_type == 'i32':
+            record_type = ["longout", "longin"];
+            record_dtyp = "asynInt32";
+        else:                   # TODO Add string handling
+            print("Invalid data type.");
+            sys.exit(1);
+
+        #First write the output record
+        for record_idx in range(2):
+            record_line = 'record({}, "{}")\n'.format(record_type[record_idx], base_pv_name+record_suffix[record_idx]);
+            param_db_file.write(record_line);
+            param_db_file.write('{\n');
+            # TODO Add in support for initial values and PINI
+            dtyp_line = 'field(DTYP, "{}")\n'.format(record_dtyp);
+            param_db_file.write(dtyp_line);
+            if record_idx == 0:
+                scan_line = 'field(SCAN, "Passive")\n';
+            elif record_idx == 1:
+                scan_line = 'field(SCAN, "I/O Intr")\n';
+            else:               # Shouldn't be here
+                print("Bad state in writing parameter database.");
+                sys.exit(1);
+
+            param_db_file.write(scan_line);
+
+            if record_idx == 0:
+                addr_line = 'field(OUT, "@asyn($(PORT),$(ADDR),$(TIMEOUT)){}")\n'.format(asyn_name);
+            elif record_idx == 1:
+                addr_line = 'field(INP, "@asyn($(PORT),$(ADDR),$(TIMEOUT)){}")\n'.format(asyn_name);
+            else:               # Shouldn't be here
+                print("Bad state in producing address line.\n");
+                sys.exit(1);
+
+            param_db_file.write(addr_line);
+            out_line = '}\n\n';
+            param_db_file.write(out_line);
+                
+            
 
 template_file.close();
 query_db_file.close();
